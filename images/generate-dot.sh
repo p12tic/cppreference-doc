0534e879a0c082c9e908627784e13377347f9a8c
p12 <tir5c3@yahoo.co.uk>
2012-05-12 12:01:08 +0300

    images: Generate image-maps in the script

--- images/generate-dot.sh
+++ images/generate-dot.sh
@@ -4,8 +4,10 @@ for i in $(find -iname "*.dot")
 do
     infile="$i"
     outfile="$i".svg
+    mapfile="$i".map
     tmpfile="$i".tmp
     dot -Tsvg -o$tmpfile $infile
+    dot -Timap $infile | sed 1d | awk '{sub(/,/, " ", $3); sub(/,/, " ", $4); print $1" "$3" "$4" [["$2"]]"; };' > $mapfile
     xsltproc --novalid fix_svg.xsl $tmpfile > $outfile
     rm $tmpfile
 done
