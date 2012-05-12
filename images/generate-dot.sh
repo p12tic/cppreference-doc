33e3b076f4402d9b6ecbdf7d6c158f10fac18bb4
p12 <tir5c3@yahoo.co.uk>
2012-05-12 12:02:55 +0300

    images: Put the results in separate directory

--- images/generate-dot.sh
+++ images/generate-dot.sh
@@ -3,9 +3,9 @@
 for i in $(find -iname "*.dot")
 do
     infile="$i"
-    outfile="$i".svg
-    mapfile="$i".map
-    tmpfile="$i".tmp
+    outfile="output/$i.svg"
+    mapfile="output/$i.map"
+    tmpfile="output/$i.tmp"
     dot -Tsvg -o$tmpfile $infile
     dot -Timap $infile | sed 1d | awk '{sub(/,/, " ", $3); sub(/,/, " ", $4); print $1" "$3" "$4" [["$2"]]"; };' > $mapfile
     xsltproc --novalid fix_svg.xsl $tmpfile > $outfile
