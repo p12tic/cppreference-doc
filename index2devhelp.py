p12tic/cppreference-doc

commit 9b020fd7e1983c584b72483edd2968720b477d25

Transform: rewrite the index transformations in python+lxml

index2devhelp.py
@@ -1,0 +1,88 @@
+#!/usr/bin/env python
+'''
+    Copyright (C) 2013  p12 <tir5c3@yahoo.co.uk>
+
+    This file is part of cppreference-doc
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+'''
+
+from index_transform import IndexTransform, xml_escape
+import sys
+
+if len(sys.argv) != 8:
+    print '''Please provide the following 7 arguments:
+ * a link to the location of the book
+ * the chapters file to include
+ * the title of the book
+ * the name of the package
+ * the link relative to the root of the documentation
+ * the file name of the source file
+ * the file name of the destination file
+'''
+
+book_base = sys.argv[1]
+chapters_fn = sys.argv[2]
+book_title = sys.argv[3]
+book_name = sys.argv[4]
+rel_link = sys.argv[5]
+in_fn = sys.argv[6]
+dest_fn = sys.argv[7]
+
+out_f = open(dest_fn, 'w')
+
+
+class Index2Devhelp(IndexTransform):
+
+    def get_mark(self, el):
+        if el.tag == 'const': return 'macro'
+        elif el.tag == 'function': return 'function'
+        elif el.tag == 'constructor': return 'function'
+        elif el.tag == 'destructor': return 'function'
+        elif el.tag == 'class': return 'class'
+        elif el.tag == 'enum': return 'enum'
+        elif el.tag == 'typedef': return 'typedef'
+        elif el.tag == 'specialization': return 'class'
+        elif el.tag == 'overload': return 'function'
+        return ''
+
+    def process_item_hook(self, el, full_name, full_link):
+        global out_f, rel_link
+        out_f.write('<keyword type="' + xml_escape(self.get_mark(el))
+                    + '" name="' + xml_escape(full_name)
+                    + '" link="' + xml_escape(rel_link) + '"/>\n')
+        IndexTransform.process_item_hook(self, el, full_name, full_link)
+
+out_f.write('<?xml version="1.0"?>\n'
+           + '<book title="' + xml_escape(book_title)
+           + '" xmlns="http://www.devhelp.net/book'
+           + '" name="' + xml_escape(book_name)
+           + '" base="' + xml_escape(book_base)
+           + '" link="' + xml_escape(rel_link)
+           + '" version="2" language="c++">\n')
+
+chapters_f = open(chapters_fn)
+out_f.write(chapters_f.read() + '\n')
+out_f.write('<functions>')
+
+tr = Index2Devhelp()
+tr.transform(in_fn)
+
+out_f.write('''
+  </functions>
+</book>
+''')
+
+
+

Makefile
@@ -39,11 +39,11 @@ DISTFILES=	\
 		devhelp2qch.xsl			\
 		fix_devhelp-links.xsl	\
 		httrack-workarounds.py	\
-		index2browser.xsl		\
-		index2devhelp.xsl		\
-		index2search.xsl		\
-		index2highlight.xsl		\
-		index_transform.xsl		\
+		index2browser.py		\
+		index2devhelp.py		\
+		index2search.py			\
+		index2highlight.py		\
+		index_transform.py		\
 		index-chapters-c.xml	\
 		index-chapters-cpp.xml	\
 		index-functions.README	\
@@ -110,21 +110,15 @@ link-map.xml: output
 
 #build the .devhelp2 index
 cppreference-doc-en-c.devhelp2: output link-map.xml
-	xsltproc --stringparam book-base $(docdir)/html 			\
-			 --stringparam chapters-file index-chapters-c.xml	\
-			 --stringparam title "C Standard Library reference"	\
-			 --stringparam name "cppreference-doc-en-c"	\
-			 --stringparam link "c"	\
-			 index2devhelp.xsl index-functions-c.xml > devhelp-index-c.xml
+	./index2devhelp.py $(docdir)/html index-chapters-c.xml 	\
+		"C Standard Library reference" "cppreference-doc-en-c" "c" \
+		index-functions-c.xml devhelp-index-c.xml
 	xsltproc fix_devhelp-links.xsl devhelp-index-c.xml > cppreference-doc-en-c.devhelp2
 
 cppreference-doc-en-cpp.devhelp2: output link-map.xml
-	xsltproc --stringparam book-base $(docdir)/html 			\
-			 --stringparam chapters-file index-chapters-cpp.xml	\
-			 --stringparam title "C++ Standard Library reference"	\
-			 --stringparam name "cppreference-doc-en-cpp"	\
-			 --stringparam link "cpp"	\
-			 index2devhelp.xsl index-functions-cpp.xml > devhelp-index-cpp.xml
+	./index2devhelp.py $(docdir)/html index-chapters-cpp.xml 	\
+		"C++ Standard Library reference" "cppreference-doc-en-cpp" "cpp" \
+		index-functions-cpp.xml devhelp-index-cpp.xml
 	xsltproc fix_devhelp-links.xsl devhelp-index-cpp.xml > cppreference-doc-en-cpp.devhelp2
 
 #build the .qch (QT help) file

index-chapters-c.xml
@@ -1,4 +1,3 @@
-<?xml version="1.0" encoding="UTF-8"?>
 <!--
     Copyright (C) 2011  p12 <tir5c3@yahoo.co.uk>
 

index-chapters-cpp.xml
@@ -1,4 +1,3 @@
-<?xml version="1.0" encoding="UTF-8"?>
 <!--
     Copyright (C) 2011  p12 <tir5c3@yahoo.co.uk>
 

index2browser.py
@@ -1,0 +1,89 @@
+#!/usr/bin/env python
+'''
+    Copyright (C) 2013  p12 <tir5c3@yahoo.co.uk>
+
+    This file is part of cppreference-doc
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+'''
+
+from index_transform import IndexTransform, xml_escape
+import sys
+
+if len(sys.argv) != 3:
+    print '''Please provide the file name of the index as the first argument
+ and the file name of the destination as the second '''
+    sys.exit(1)
+
+out_f = open(sys.argv[2], 'w')
+
+class Index2Browser(IndexTransform):
+
+    def output_item(self, el, full_name, full_link):
+        mark = ''
+        if el.tag == 'const': mark = '(const)'
+        elif el.tag == 'function': mark = '(function)'
+        elif el.tag == 'constructor': mark = '(function)'
+        elif el.tag == 'destructor': mark = '(function)'
+        elif el.tag == 'class': mark = '(class)'
+        elif el.tag == 'enum': mark = '(enum)'
+        elif el.tag == 'typedef': mark = '(typedef)'
+        elif el.tag == 'specialization': mark = '(class)'
+        elif el.tag == 'overload': mark = '(function)'
+
+        res = u''
+        res += '<tt><b>' + xml_escape(full_name) + '</b></tt> [<span class="link">'
+        res += '<a href="http://en.cppreference.com/w/' + xml_escape(full_link) + '">'
+        res += full_link + '</a></span>] <span class="mark">' + mark + '</span>\n'
+        return res
+
+    def process_item_hook(self, el, full_name, full_link):
+        global out_f
+        out_f.write('<li>' + self.output_item(el, full_name, full_link) + '<ul>')
+        IndexTransform.process_item_hook(self, el, full_name, full_link)
+        out_f.write('</ul></li>\n')
+
+out_f.write('''
+<html>
+  <head>
+  <style type="text/css">
+    body {
+      font-size: 0.8em;
+    }
+
+    .link a {
+      font-size: 0.8em;
+      color: #808080;
+    }
+    .mark {
+      font-size: 0.8em;
+      color: #008000;
+    }
+  </style>
+  </head>
+  <body>
+    <ul>
+''')
+
+tr = Index2Browser()
+tr.transform(sys.argv[1])
+
+out_f.write('''
+    </ul>
+  </body>
+</html>
+''')
+
+
+

index2browser.xsl
@@ -1,102 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!--
-    Copyright (C) 2011  p12 <tir5c3@yahoo.co.uk>
-
-    This file is part of cppreference-doc
-    
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see http://www.gnu.org/licenses/.
--->
-<xsl:stylesheet version="1.0"
-                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
-                xmlns:str="http://exslt.org/strings"
-                >
-<xsl:import href="index_transform.xsl"/>
-
-<xsl:template match="/index">
-  <html>
-  <head>
-  <style type="text/css">
-    body {
-      font-size: 0.8em;
-    }
-
-    .link a {
-      font-size: 0.8em;
-      color: #808080;
-    }
-    .mark {
-      font-size: 0.8em;
-      color: #008000;
-    }
-  </style>
-  </head>
-  <body>
-    <ul>
-      <xsl:apply-templates mode="process-item" select="child::*"/>
-    </ul>
-  </body>
-  </html>
-</xsl:template>
-
-<xsl:template name="process-item-finalize">
-  <xsl:param name="name"/>
-  <xsl:param name="link"/>
-  <li>
-    <xsl:call-template name="output-item">
-      <xsl:with-param name="name" select="$name"/>
-      <xsl:with-param name="link" select="$link"/>
-    </xsl:call-template>
-
-    <ul>
-      <xsl:apply-templates mode="process-children" select=".">
-        <xsl:with-param name="parent-name" select="$name"/>
-        <xsl:with-param name="parent-link" select="$link"/>
-      </xsl:apply-templates>
-    </ul>
-  </li>
-</xsl:template>
-
-<xsl:template name="output-item">
-  <xsl:param name="name"/>
-  <xsl:param name="link"/>
-  
-  <xsl:variable name="mark">
-    <xsl:choose>
-      <xsl:when test="name()='const'"><xsl:text>(const)</xsl:text></xsl:when>
-      <xsl:when test="name()='function'"><xsl:text>(function)</xsl:text></xsl:when>
-      <xsl:when test="name()='constructor'"><xsl:text>(function)</xsl:text></xsl:when>
-      <xsl:when test="name()='destructor'"><xsl:text>(function)</xsl:text></xsl:when>
-      <xsl:when test="name()='class'"><xsl:text>(class)</xsl:text></xsl:when>
-      <xsl:when test="name()='enum'"><xsl:text>(enum)</xsl:text></xsl:when>
-      <xsl:when test="name()='typedef'"><xsl:text>(typedef)</xsl:text></xsl:when>
-      <xsl:when test="name()='specialization'"><xsl:text>(class)</xsl:text></xsl:when>
-      <xsl:when test="name()='overload'"><xsl:text>(function)</xsl:text></xsl:when>
-      <xsl:otherwise/>
-    </xsl:choose>
-  </xsl:variable>
-  
-  <tt><b><xsl:value-of select="$name"/></b></tt>
-  [
-  <span class="link">
-    <a href="http://en.cppreference.com/w/{$link}">
-      <xsl:value-of select="$link"/>
-    </a>
-  </span>
-  ]
-  <span class="mark"><xsl:value-of select="$mark"/></span>
-</xsl:template>
-
-</xsl:stylesheet>
-
-

index2devhelp.xsl
@@ -1,67 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!--
-    Copyright (C) 2011  p12 <tir5c3@yahoo.co.uk>
-
-    This file is part of cppreference-doc
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see http://www.gnu.org/licenses/.
--->
-<xsl:stylesheet version="1.0"
-                xmlns="http://www.devhelp.net/book"
-                xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
-                >
-<xsl:import href="index_transform.xsl"/>
-<xsl:param name="book-base" select="'/usr/share/doc/cppreference-doc-en/html'"/>
-<xsl:param name="chapters-file" select="''"/>
-<xsl:param name="title" select="''"/>
-<xsl:param name="name" select="''"/>
-<xsl:param name="link" select="''"/>
-
-<xsl:output indent="yes"/>
-
-<xsl:template match="/index">
-  <book title="{$title}" name="{$name}"
-        base="{$book-base}" link="{$link}" version="2" language="c++">
-    <xsl:copy-of select="document($chapters-file)"/>
-    <functions>
-      <xsl:apply-templates mode="process-item" select="child::*"/>
-    </functions>
-  </book>
-</xsl:template>
-
-<xsl:template name="output-item">
-  <xsl:param name="name"/>
-  <xsl:param name="link"/>
-
-  <xsl:variable name="mark">
-    <xsl:choose>
-      <xsl:when test="name()='const'"><xsl:text>macro</xsl:text></xsl:when>
-      <xsl:when test="name()='function'"><xsl:text>function</xsl:text></xsl:when>
-      <xsl:when test="name()='constructor'"><xsl:text>function</xsl:text></xsl:when>
-      <xsl:when test="name()='destructor'"><xsl:text>function</xsl:text></xsl:when>
-      <xsl:when test="name()='class'"><xsl:text>class</xsl:text></xsl:when>
-      <xsl:when test="name()='enum'"><xsl:text>enum</xsl:text></xsl:when>
-      <xsl:when test="name()='typedef'"><xsl:text>typedef</xsl:text></xsl:when>
-      <xsl:when test="name()='specialization'"><xsl:text>class</xsl:text></xsl:when>
-      <xsl:when test="name()='overload'"><xsl:text>function</xsl:text></xsl:when>
-      <xsl:otherwise/>
-    </xsl:choose>
-  </xsl:variable>
-
-  <keyword type="{$mark}" name="{$name}" link="{$link}"/>
-</xsl:template>
-
-</xsl:stylesheet>
-
-

index2highlight.py
@@ -1,0 +1,54 @@
+#!/usr/bin/env python
+'''
+    Copyright (C) 2013  p12 <tir5c3@yahoo.co.uk>
+
+    This file is part of cppreference-doc
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+'''
+
+from index_transform import IndexTransform
+import sys
+
+if len(sys.argv) != 3:
+    print '''Please provide the file name of the index as the first argument
+ and the file name of the destination as the second '''
+    sys.exit(1)
+
+out_f = open(sys.argv[2], 'w')
+
+class Index2Highlight(IndexTransform):
+
+    def process_item_hook(self, el, full_name, full_link):
+        global out_f
+
+        if el.getparent().tag != 'index' and (
+            el.tag == 'function' or el.tag == 'constructor' or el.tag == 'destructor'): pass
+        elif '<' in full_name: pass
+        elif '>' in full_name: pass
+        elif '(' in full_name: pass
+        elif ')' in full_name: pass
+        else:
+            out_f.write(full_name + ' => ' + full_link + '\n')
+
+        IndexTransform.process_item_hook(self, el, full_name, full_link)
+
+    def inherits_worker(self, parent_name, pending, finished=list()):
+        pass # do not walk the inheritance hierarchy
+
+tr = Index2Highlight()
+tr.transform(sys.argv[1])
+
+
+

commit e5446b8c28b8fffc5d22fbd4d301aaa69ede3299

Fix keyword index generation for qch
Previously due this bug all C++ context searches were pointing to "cpp" index
page, C searched to "c" index page. This fix brings back context search (F1)
functionality that was broken for last 2014 qch releases.

This bug was introduced by 9b020fd7e1983c584b72483edd2968720b477d25 rewrite of
index generation from XSL to Python.

index2devhelp.py
@@ -61,7 +61,7 @@ class Index2Devhelp(IndexTransform):
         global out_f, rel_link
         out_f.write('<keyword type="' + xml_escape(self.get_mark(el))
                     + '" name="' + xml_escape(full_name)
-                    + '" link="' + xml_escape(rel_link) + '"/>\n')
+                    + '" link="' + xml_escape(full_link) + '"/>\n')
         IndexTransform.process_item_hook(self, el, full_name, full_link)
 
 out_f.write('<?xml version="1.0"?>\n'