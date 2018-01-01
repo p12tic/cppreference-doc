5b0b58f1a3a34b25e22784b2917226ce7f1e35d3
Povilas Kanapickas <povilas@radix.lt>
2018-01-01 17:58:55 +0200

    Gadgets/Stdrev: Add license comments to tests

--- gadgets/standard_revisions-tests/rev-inl-works-in-text.py
+++ gadgets/standard_revisions-tests/rev-inl-works-in-text.py
@@ -1,4 +1,22 @@
-# -*- coding: utf-8 -*-
+'''
+    Copyright (C) 2017-2018  Povilas Kanapickas <povilas@radix.lt>
+
+    This file is part of cppreference.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+'''
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
